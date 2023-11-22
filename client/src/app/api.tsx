3c82566a5ab93c7af2923f5d839b9509908e6d98
weslex555 <wgcoburn@outlook.com>
2023-11-22 13:27:22 -0800

    fixed server path

--- client/src/app/api.tsx
+++ client/src/app/api.tsx
@@ -1,5 +1,5 @@
 const serverURL = import.meta.env.VITE_SERVER_URL;
-const apiURL = (path: string) => `${serverURL}${path}`;
+const apiURL = (path: string) => `${serverURL}/${path}`;
 
 const API = {
   getAllNotes: () => apiURL('note'),
