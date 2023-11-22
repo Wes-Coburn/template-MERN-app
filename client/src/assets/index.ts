96ebdf324d011640ef607520ceb652737c331330
weslex555 <wgcoburn@outlook.com>
2023-11-22 11:39:29 -0800

    Update index.ts

--- client/src/assets/index.ts
+++ client/src/assets/index.ts
@@ -1,6 +1,6 @@
 /* eslint-disable import/no-absolute-path */
-import favIcon from '/assets/logo.svg';
-import screenshot1 from '/assets/screenshot1.png';
+import favIcon from '/assets_public/logo.svg';
+import screenshot1 from '/assets_public/screenshot1.png';
 
 const ASSETS = {
   images: {
