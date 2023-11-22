d6647225194f533199be435419c6662e7f3730b1
weslex555 <wgcoburn@outlook.com>
2023-11-22 13:22:03 -0800

    try fix undefined public url

--- client/src/app/routes.ts
+++ client/src/app/routes.ts
@@ -1,11 +1,11 @@
-const publicURL = `${import.meta.env.VITE_PUBLIC_URL}/`;
-const protocol = 'https://';
+const publicURL = () => `${import.meta.env.VITE_PUBLIC_URL}/`;
+const protocol = () => 'https://';
 // const mobileDomain = 'm.';
 
 export const CANONICAL = {
-  desktop: (path: string) => encodeURI(`${protocol}${publicURL}${path}`),
+  desktop: (path: string) => encodeURI(`${protocol()}${publicURL()}${path}`),
   /* mobile: (path: string) =>
-    encodeURI(`${protocol}${mobileDomain}${publicURL}${path}`),
+    encodeURI(`${protocol()}${mobileDomain}${publicURL()}${path}`),
   */
 };
 
